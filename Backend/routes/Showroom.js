387b193bed799cf1b25c6d7d8b6ff1fb8c1e7096
abyan-yusuf <98206561+abyan-yusuf@users.noreply.github.com>
2024-10-17 22:12:12 +0600

    A lil correction

--- Backend/routes/Showroom.js
+++ Backend/routes/Showroom.js
@@ -6,17 +6,19 @@ import {
   getNearbyShowrooms,
   updateShowroom,
 } from "../controllers/Showroom.js";
+import { isAdmin, requireSignIn } from "../middlewares/Auth.js";
 
 const router = Router();
 
-router.post("/create", createShowroom);
+router.post("/create", requireSignIn
+  , isAdmin, createShowroom);
 
 router.get("/all-showrooms", getAllShowrooms);
 
 router.post("/nearby", getNearbyShowrooms);
 
-router.delete("/delete/:id", deleteShowroom);
+router.delete("/delete/:id", requireSignIn, isAdmin, deleteShowroom);
 
-router.put("/update/:id", updateShowroom);
+router.put("/update/:id", requireSignIn, isAdmin, updateShowroom);
 
 export default router;
